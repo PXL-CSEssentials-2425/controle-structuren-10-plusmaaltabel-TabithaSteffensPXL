f3e7a4c217949b6e7ac5798e8d9b7965f437dcb2
Tabitha Steffens <tabitha.steffens@student.pxl.be>
2024-10-15 19:10:08 +0200

    eerste versie, heb geprobeerd om een horizontale scrollbar toe te voegen maar die gaat nog niet

---
+++ plusmaaltabel/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace plusmaaltabel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultTextBox.Clear();
+            maximumTextBox.Clear();
+            maximumTextBox.Focus();
+        }
+
+        private void plusButton_Click(object sender, RoutedEventArgs e)
+        {
+           if (int.TryParse(maximumTextBox.Text, out int maximum))
+            {
+                if (maximum > 20)
+                {
+                    maximum = 10;
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            
+            for (int row = 0; row <= maximum; row++)
+            {
+                for (int col = 0; col <= maximum; col++)
+                {
+                    if (col == 0)
+
+                        sb.Append($"{row}\t");
+                    else
+                        sb.Append($"{row + col}\t");
+                }
+                sb.AppendLine();
+            }
+            resultTextBox.Text = sb.ToString();
+            
+        }
+
+        private void multiplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(maximumTextBox.Text, out int maximum))
+            {
+                if (maximum > 20)
+                {
+                    maximum = 10;
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+
+            for (int row = 0; row <= maximum; row++)
+            {
+                for (int col = 0; col <= maximum; col++)
+                {
+                    if (col == 0)
+
+                        sb.Append($"{row}\t");
+                    else
+                        sb.Append($"{row * col}\t");
+                }
+                sb.AppendLine();
+            }
+            resultTextBox.Text = sb.ToString();
+        }
+    }
+}
